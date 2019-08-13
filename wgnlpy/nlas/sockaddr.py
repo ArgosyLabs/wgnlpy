31c3465cc97ad7bb938964f79ad82b28993e3063
Derrick Lyndon Pallas <derrick@argosylabs.com>
2019-08-13 23:16:31 +0000

    nlas/sockaddr: frob more
    Expand the types of things we will frob into a sockaddr, falling back to the
    sockaddr helper introduced earlier.  In most cases, attempt to preserve the
    default port.

--- wgnlpy/nlas/sockaddr.py
+++ wgnlpy/nlas/sockaddr.py
@@ -5,6 +5,7 @@ from struct import unpack_from
 from socket import AF_INET, AF_INET6, getaddrinfo
 from urllib.parse import urlparse
 from pyroute2.netlink import nla_base
+from ..sockaddr import sockaddr as to_sa
 from ..sockaddr_in import sockaddr_in
 from ..sockaddr_in6 import sockaddr_in6
 
@@ -40,20 +41,30 @@ class sockaddr(nla_base):
         DEFAULT_PORT = 51820
         if isinstance(nitz, (sockaddr_in, sockaddr_in6)):
             return nitz
+        elif isinstance(nitz, (list, tuple)):
+            fields = ('addr', 'port', 'flowinfo', 'scope_id', '')
+            kwargs = { 'port': DEFAULT_PORT }
+            kwargs.update(dict(zip(fields, nitz)))
+            return to_sa(**kwargs)
+        elif isinstance(nitz, dict):
+            if 'port' in nitz:
+                return to_sa(**nitz)
+            else:
+                return to_sa(port=DEFAULT_PORT, **nitz)
         elif isinstance(nitz, str):
             url = urlparse("//" + nitz)
             family, *meh, sockaddr = getaddrinfo(url.hostname, url.port or DEFAULT_PORT)[0]
 
             try:
                 type, *fields = {
-                    AF_INET: (sockaddr_in, 'addr', 'port'),
-                    AF_INET6: (sockaddr_in6, 'addr', 'port', 'flowinfo', 'scope_id'),
+                    AF_INET: (sockaddr_in, 'addr', 'port', ''),
+                    AF_INET6: (sockaddr_in6, 'addr', 'port', 'flowinfo', 'scope_id', ''),
                 }[family]
             except:
                 raise NotImplementedError
 
             return type(**dict(zip(fields, sockaddr)))
         else:
-            raise NotImplementedError
+            return to_sa(addr=nitz, port=DEFAULT_PORT)
 
 #
