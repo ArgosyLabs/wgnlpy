1149f76488923c84fc4094c42d9f9b480bf76915
Derrick Pallas <derrick@pallas.us>
2020-06-18 13:41:53 -0700

    example: extend to reset interface key, use preshared key for peer
    This change just extends the example to flex more of wgnlpy's features.

--- example.py
+++ example.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # SPDX-License-Identifier: Unlicense
 
-from wgnlpy import WireGuard, PrivateKey
+from wgnlpy import WireGuard, PrivateKey, PresharedKey
 from base64 import b64encode
 from pprint import pprint
 
@@ -11,7 +11,10 @@ print("PEER", repr(peer))
 wg = WireGuard()
 interface = "wg-test"
 
+wg.set_interface(interface, private_key=PrivateKey.generate(), replace_peers=True)
+
 wg.set_peer(interface, peer,
+    preshared_key=PresharedKey.generate(),
     endpoint="[::ffff:203.0.113.0%8]:12345",
     allowedips=["2001:db8::/32", "198.51.100.1"],
     )
