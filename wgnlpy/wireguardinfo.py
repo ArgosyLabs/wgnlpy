caaf255d5063ce7dfa26b0cb2a6b7712955a688f
Derrick Pallas <derrick@pallas.us>
2020-06-23 00:22:25 -0700

    wgnlpy/wireguardinfo: encapsulate peer info in WireGuardPeer object
    In the same way that we pass back a WireGuardInfo object, do the same with
    peers.  Note that this is an API breaking change, since info about the peer
    was accessed via dictionary and is now accessed via attributes.

--- wgnlpy/wireguardinfo.py
+++ wgnlpy/wireguardinfo.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 # SPDX-License-Identifier: MIT
 
-from .preshared_key import PresharedKey
 from .private_key import PrivateKey
 from .public_key import PublicKey
+from .wireguardpeer import WireGuardPeer
 
 class WireGuardInfo(object):
     __slots__ = (
@@ -33,32 +33,10 @@ class WireGuardInfo(object):
         self.peers = { }
 
         for message in messages:
-            for peer in message.get_attr('WGDEVICE_A_PEERS') or []:
-                public_key = peer.get_attr('WGPEER_A_PUBLIC_KEY')
-                assert public_key is not None
-                if public_key is not None:
-                    public_key = PublicKey(public_key)
-                if public_key not in self.peers:
-                    preshared_key = peer.get_attr('WGPEER_A_PRESHARED_KEY')
-                    if not spill_preshared_keys:
-                        preshared_key = preshared_key is not None
-                    elif preshared_key is not None:
-                        preshared_key = PresharedKey(preshared_key)
-                    last_handshake_time = peer.get_attr('WGPEER_A_LAST_HANDSHAKE_TIME')
-                    if not last_handshake_time > 0:
-                        last_handshake_time = None
-                    self.peers[public_key] = {
-                        'preshared_key': preshared_key,
-                        'endpoint': peer.get_attr('WGPEER_A_ENDPOINT'),
-                        'persistent_keepalive_interval': peer.get_attr('WGPEER_A_PERSISTENT_KEEPALIVE_INTERVAL'),
-                        'last_handshake_time': last_handshake_time,
-                        'rx_bytes': peer.get_attr('WGPEER_A_RX_BYTES'),
-                        'tx_bytes': peer.get_attr('WGPEER_A_TX_BYTES'),
-                        'allowedips': [],
-                        'protocol_version': peer.get_attr('WGPEER_A_PROTOCOL_VERSION'),
-                    }
-                for allowedip in peer.get_attr('WGPEER_A_ALLOWEDIPS') or []:
-                    self.peers[public_key]['allowedips'].append(allowedip.network())
+            wgp = lambda p: WireGuardPeer(p, spill_preshared_keys)
+            for peer in map(wgp, message.get_attr('WGDEVICE_A_PEERS') or []):
+                assert peer.public_key not in self.peers
+                self.peers[peer.public_key] = peer
 
     def __repr__(self):
         return repr({
---
+++ wgnlpy/wireguardpeer.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# SPDX-License-Identifier: MIT
+
+from .preshared_key import PresharedKey
+from .public_key import PublicKey
+
+class WireGuardPeer(object):
+    __slots__ = (
+        "public_key",
+        "preshared_key",
+        "endpoint",
+        "persistent_keepalive_interval",
+        "last_handshake_time",
+        "rx_bytes",
+        "tx_bytes",
+        "allowedips",
+        "protocol_version",
+    )
+
+    def __init__(self, peer, spill_preshared_keys=False):
+        self.public_key = PublicKey(peer.get_attr('WGPEER_A_PUBLIC_KEY'))
+        self.preshared_key = peer.get_attr('WGPEER_A_PRESHARED_KEY')
+        if not spill_preshared_keys:
+            self.preshared_key = self.preshared_key is not None
+        else:
+            self.preshared_key = PresharedKey(self.preshared_key)
+        self.endpoint = peer.get_attr('WGPEER_A_ENDPOINT')
+        self.persistent_keepalive_interval = peer.get_attr('WGPEER_A_PERSISTENT_KEEPALIVE_INTERVAL')
+        self.last_handshake_time = peer.get_attr('WGPEER_A_LAST_HANDSHAKE_TIME')
+        if not self.last_handshake_time > 0:
+            self.last_handshake_time = None
+        self.rx_bytes = peer.get_attr('WGPEER_A_RX_BYTES')
+        self.tx_bytes = peer.get_attr('WGPEER_A_TX_BYTES')
+        self.protocol_version = peer.get_attr('WGPEER_A_PROTOCOL_VERSION')
+
+        self.allowedips = []
+        for allowedip in peer.get_attr('WGPEER_A_ALLOWEDIPS') or []:
+            self.allowedips.append(allowedip.network())
+
+    def __repr__(self):
+        return repr({
+            'public_key': self.public_key,
+            'preshared_key': self.preshared_key,
+            'endpoint': self.endpoint,
+            'persistent_keepalive_interval': self.persistent_keepalive_interval,
+            'last_handshake_time': self.last_handshake_time,
+            'rx_bytes': self.rx_bytes,
+            'tx_bytes': self.tx_bytes,
+            'allowedips': self.allowedips,
+            'protocol_version': self.protocol_version,
+        })
