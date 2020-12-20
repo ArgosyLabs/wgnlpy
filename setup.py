4630a1273d4afca75c149632e3844e68e59861d5
Derrick Lyndon Pallas <derrick@pallas.us>
2020-12-20 10:38:53 -0800

    v0.1.4

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name = "wgnlpy",
-    version = "0.1.3",
+    version = "0.1.4",
     description = ("Netlink connector to WireGuard"),
     url = "https://github.com/ArgosyLabs/wgnlpy",
     author = "Derrick Lyndon Pallas",
